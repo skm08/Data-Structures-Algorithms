bd2a6cdef48a2239ea2a22061f3d6242a3194b6f
Sagor Kumar Mitra <138737856+skm08@users.noreply.github.com>
2025-02-18 00:00:27 +0600

    Time: 38 ms (58.27%) | Memory: 18 MB (49.32%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,15 +1,30 @@
 class Solution:
-    def buildString(self, freq: list[int]) -> int:
-        ways = 0
-        for i in range(26):
-            if freq[i] > 0:
-                freq[i] -= 1
-                ways += 1 + self.buildString(freq)
-                freq[i] += 1
-        return ways
-
     def numTilePossibilities(self, tiles: str) -> int:
-        freq = [0] * 26
-        for c in tiles:
-            freq[ord(c) - ord('A')] += 1
-        return self.buildString(freq)
+        count = Counter(tiles)
+
+        def backtrack():
+            res = 0
+            for c in count:
+                if count[c] > 0:
+                    count[c] -= 1
+                    res += 1
+                    res += backtrack()
+                    count[c] += 1
+            return res
+        return backtrack()
+
+# class Solution:
+#     def buildString(self, freq: list[int]) -> int:
+#         ways = 0
+#         for i in range(26):
+#             if freq[i] > 0:
+#                 freq[i] -= 1
+#                 ways += 1 + self.buildString(freq)
+#                 freq[i] += 1
+#         return ways
+
+#     def numTilePossibilities(self, tiles: str) -> int:
+#         freq = [0] * 26
+#         for c in tiles:
+#             freq[ord(c) - ord('A')] += 1
+#         return self.buildString(freq)
