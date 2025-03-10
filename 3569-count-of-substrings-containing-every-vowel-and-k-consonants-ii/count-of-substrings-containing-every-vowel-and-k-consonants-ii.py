6ae4d40a6fc7df61b67a935a6501d3af5f2adb33
Sagor Kumar Mitra <138737856+skm08@users.noreply.github.com>
2025-03-10 23:31:18 +0600

    Time: 1681 ms (71.10%) | Memory: 25.4 MB (13.87%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,40 +1,51 @@
 class Solution:
+    def isVowel(self, c: str) -> bool:
+        return c in {'a', 'e', 'i', 'o', 'u'}
+
+    def findNextConsonant(self, word: str, next_consonant: list) -> None:
+        n = len(word)
+        next = n
+        for i in range(n - 1, -1, -1):
+            next_consonant[i] = next
+            if not self.isVowel(word[i]):
+                next = i
+
     def countOfSubstrings(self, word: str, k: int) -> int:
-        res = cons = left = cur = 0
-        freq = defaultdict(int)
-        vowel = 'aeiou'
+        n = len(word)
+        next_consonant = [0] * n
+        self.findNextConsonant(word, next_consonant)
 
-        for ch in word:
-            # count vowel frequency and number of consonants in the current window
-            if ch in vowel:
-                freq[ch] += 1
-            else: 
-                cons += 1
-                # reset number of valid substring found in the current window since now it is no more valid
-                cur = 0 
+        count = 0
+        consonants = 0
+        vowel_freq = {}
+        left = 0
+        right = 0
+        prev = -1
 
-            # shrink until we have the right amount of consonants
-            while cons > k:
-                if word[left] not in vowel:
-                    cons -= 1
-                elif freq[word[left]] == 1:
-                    del freq[word[left]]
+        while right < n:
+            if prev != right:
+                if self.isVowel(word[right]):
+                    vowel_freq[word[right]] = vowel_freq.get(word[right], 0) + 1
                 else:
-                    freq[word[left]] -= 1
-                left += 1
+                    consonants += 1
+                prev = right
 
-            # if we have the right amount of consonants and vowels, shrink current window as much as possible
-            # each step is a probable substring if we keep finding vowels on the right
-            while len(freq) == 5 and cons == k:
-                cur += 1
-                if word[left] not in vowel:
-                    cons -= 1
-                elif freq[word[left]] == 1:
-                    del freq[word[left]]
+            if right >= (5 + k - 1):
+                if len(vowel_freq) == 5 and consonants == k:
+                    count += next_consonant[right] - right
+
+                # Move left ptr to right: Shrink window
+                if (len(vowel_freq) == 5 and consonants == k) or (consonants > k):
+                    if self.isVowel(word[left]):
+                        vowel_freq[word[left]] -= 1
+                        if vowel_freq[word[left]] == 0:
+                            del vowel_freq[word[left]]
+                    else:
+                        consonants -= 1
+                    left += 1
                 else:
-                    freq[word[left]] -= 1
-                left += 1
-            res += cur # sum the overall number of substring in the current window
-        return res
+                    right += 1
+            else:
+                right += 1
 
-            
+        return count
