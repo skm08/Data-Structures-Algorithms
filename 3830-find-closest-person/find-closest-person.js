fc8200cb75f3c5064be1ab868436dfde6195f079
Sagor Kumar Mitra <138737856+skm08@users.noreply.github.com>
2025-09-05 02:17:02 +0600

    Time: 0 ms (100.00%) | Memory: 55 MB (76.61%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    if (Math.abs(x-z) < Math.abs(y-z)) {
+        return 1;
+    }
+    else if (Math.abs(x-z) > Math.abs(y-z)) {
+        return 2;
+    }
+    else return 0;
+};
